4343b22ac4dad79e8400c0a00f8ff8f78bcf0118
Evgenia Semez <esemez@indeed.com>
2019-04-27 10:02:04 -0700

    creating function moviethis

--- liri.js
+++ liri.js
@@ -29,19 +29,19 @@ switch (action) {
         dowhatitsays();
         break;
 }
-
+// concert-this "Billie Eilish" - example
 function concertthis() {
     axios.get("https://rest.bandsintown.com/artists/" + value + "/events?app_id=codingbootcamp").then(
         function (response) {
             for (var i = 0; i < response.data.length; i++) {
                 var venueName = response.data[i].venue.name;
-                console.log(venueName);
+                console.log("Venue Name: " + venueName);
                 var country = response.data[i].venue.country;
-                console.log(country);
+                console.log("Country: " + country);
                 var city = response.data[i].venue.city;
-                console.log(city);
+                console.log("City: " + city);
                 var date = moment(response.data[i].datetime).format();
-                console.log(date);
+                console.log("Date: " + date);
             }
         },
 
@@ -60,7 +60,7 @@ function concertthis() {
     );
 
 }
-
+// spotify-this-song "Love Yourself" - example
 function spotifythissong() {
 
     spotify.search({
@@ -71,41 +71,41 @@ function spotifythissong() {
             return console.log('Error occurred: ' + err);
         }
         var artist = data.tracks.items[0].album.artists[0].name;
-        console.log(artist);
+        console.log("Artist: " + artist);
         var song = data.tracks.items[0].name;
-        console.log(song);
+        console.log("Song: " + song);
         var link = data.tracks.items[0].external_urls.spotify;
-        console.log(link);
+        console.log("Link: " + link);
         var album = data.tracks.items[0].album.name;
-        console.log(album);
+        console.log("Album: " + album);
 
     });
 
 };
-
+// movie-this Mr.Nobody - example
 function moviethis() {
     axios.get("http://www.omdbapi.com/?apikey=trilogy&t=" + value).then(
         function (response) {
             console.log(response.data);
             var Title = response.data.Title;
-            console.log(Title);
+            console.log("Title: " + Title);
             var Year = response.data.Year;
-            console.log(Year);
-            for (var i = 0; i < Ratings.Length; i++){
-                // for each Rating{
-                
-                // }
-                var Ratings = response.data.Ratings[i].Source[0];
-                console.log(Ratings);
+            console.log("Year: " + Year);
+            for (var i = 0; i < response.data.Ratings.length; i++) {
+                var Ratings = response.data.Ratings[i].Source;
+                var Value = response.data.Ratings[i].Value;
+                if (Ratings === "Internet Movie Database" || Ratings === "Rotten Tomatoes") {
+                    console.log("Rating: " + Ratings + " Value: " + Value);
+                }
             }
             var Country = response.data.Country;
-            console.log(Country);
+            console.log("Country: " + Country);
             var Language = response.data.Language;
-            console.log(Language);
+            console.log("Language: " + Language);
             var Plot = response.data.Plot;
-            console.log(Plot);
+            console.log("Plot: " + Plot);
             var Actors = response.data.Actors;
-            console.log(Actors);
+            console.log("Actors: " + Actors);
         },
 
         function (error) {
@@ -122,7 +122,35 @@ function moviethis() {
         }
     );
 }
-
+// do-what-it-says spotify-this-song,"I Want it That Way" - example
 function dowhatitsays() {
 
+    fs.readFile("random.txt", "utf8", function (err, data) {
+        if (err) {
+            return console.log(err);
+        }
+        data = data.split(',');
+        if (data === "concert-this") {
+            concertThis();
+        } else if (data === "spotify-this-song") {
+            spotifyThisSong();
+        } else if (data === "movie-this") {
+            movieThis();
+        }
+    })
+    fs.appendFile("random.txt", text, function (err) {
+
+        // If an error was experienced we will log it.
+        if (err) {
+            console.log(err);
+        }
+
+        // If no error is experienced, we'll log the phrase "Content Added" to our node console.
+        else {
+            console.log("Content Added!");
+        }
+
+    });
 }
+
+
